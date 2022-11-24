de73585433479d0a239b4d3e0dda2e5aca83e188
Shiirudo <96832033+leshiirudo@users.noreply.github.com>
2022-11-24 22:41:48 +0100

    Write first version of bounce.py
    This adds two completed versions of the exercise's solution; one using a while loop (as expected from course) and one using a for loop.

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,18 @@
 # bounce.py
 #
 # Exercise 1.5
+
+bounceRatio = 3/5
+height = 100
+
+# using for loop
+"""for i in range(10):
+    height = height * bounceRatio
+    print(i, round(height, 4))"""
+
+# using while loop
+bounce = 1
+while bounce < 10:
+    height = height * bounceRatio
+    print(bounce, round(height, 4))
+    bounce += 1
